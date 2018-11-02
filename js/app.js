6d3e8524ad9f5915117a5935181823038507a9b9
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-11-02 02:34:11 -1000

    pau

--- js/app.js
+++ js/app.js
@@ -6,6 +6,15 @@ var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.ra
   var jue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
   document.body.style.color = jue;
 
-  
+  var tree =  'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  document.getElementById('groovy').style.color = tree; 
+
+  var four = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  document.getElementById('wicked').style.color = four; 
+
+  var pic = document.getElementById('peace');
+  if(pic.style.display === 'block'){
+      pic.style.display = 'none'
+  }else{pic.style.display ='block'}
 }
 

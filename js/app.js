1279c52a3e2c3e67de9a8022419d5aba7195d989
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-11-02 02:29:43 -1000

    almost pau

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
 function booya(){
-    // document.body.style.backgroundColor="red";
-//     var letters = '0123456789ABCDEF';
-//     var colors  = '#';
-//     for(var i=0; i<6; i++){
-//         colors += letters[Math.floor(Math.random()*16)];
-//         return colors;
-// }
+ 
 var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
   document.body.style.backgroundColor = hue;
 
   var jue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
   document.body.style.color = jue;
+
+  
 }
 

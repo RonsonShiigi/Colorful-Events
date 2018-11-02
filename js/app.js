dc763c3039752b0a4faddea62f9740c070f1afd9
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-11-02 01:52:33 -1000

    funk pau, make nice

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
 function booya(){
-    document.body.style.backgroundColor="red";
+    // document.body.style.backgroundColor="red";
+//     var letters = '0123456789ABCDEF';
+//     var colors  = '#';
+//     for(var i=0; i<6; i++){
+//         colors += letters[Math.floor(Math.random()*16)];
+//         return colors;
+// }
+var hue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  document.body.style.backgroundColor = hue;
+
+  var jue = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  document.body.style.color = jue;
 }
 
